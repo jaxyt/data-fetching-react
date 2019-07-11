733c97467c1e41ceb5b52b22db0bbb8aec2c79a5
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-07-11 19:59:13 -0400

    dope gif searching app complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      gifs: [],
+      loading: true
+    }
+  } 
+
+  /**
+   * componentDidMount() {
+   *  fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+   *    .then(res => res.json())
+   *    .then(resData => {
+   *      this.setState({gifs: resData.data});
+   *    })
+   *    .catch(error => {
+   *      console.log('Error fetching and parsing data: ', error);
+   *    })
+   * }     
+  */
+
+
+  componentDidMount() {
+    // axios.get('http://api.giphy.com/v1/gifs/trending?limit=24&api_key=dc6zaTOxFJmzC')
+    //   .then( (res) => {
+    //     this.setState({gifs: res.data.data});
+    //   })
+    //   .catch( (err) => {
+    //     console.log('Error fetching and parsing data', err);
+    //   })
+
+    this.performSearch();
+  }
+
+  performSearch = (query = "cats") => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+      .then( (res) => {
+        this.setState({gifs: res.data.data, loading: false});
+      })
+      .catch( (err) => {
+        console.log('Error fetching and parsing data', err);
+      })
+  }
+
+  render() { 
+    console.log(this.state.gifs);
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm onSearch={this.performSearch}/>      
+          </div>   
+        </div>    
+        <div className="main-content">
+          {
+            (this.state.loading)
+            ? <p>Loading...</p>
+            : <GifList data={this.state.gifs}/>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
